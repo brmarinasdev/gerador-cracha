6228b1aafecdeed20b46a5009f7819ed7b2f0fdd
Márcio Gonçalves <brmarinasdev@brmarinas.com.br>
2025-04-03 16:57:20 -0300

    Inclusão do Layout da JL
    Realizada a inclusão do Layout da JL Bracuhy.
    Para isso, foi incluído dois botões rádios para alterar entre os layouts.
    Foi feitas pequenas alterações no CSS, e inclusão de um background image.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-// Seleciona os elementos do formulário
+//Seleciona os elementos do formulário
+const radio01 = document.getElementById('radio01')
+const radio02 = document.getElementById('radio02')
 const inputFoto = document.getElementById('input-foto');
 const inputNome = document.getElementById('input-nome');
 const inputMatricula = document.getElementById('input-matricula');
@@ -8,13 +10,13 @@ const apagaFoto = document.querySelector('#input-foto');
 const apagaNome = document.querySelector('#input-nome');
 const apagaMatricula = document.querySelector('#input-matricula');
 
-// Seleciona os elementos do crachá
+//Seleciona os elementos do crachá
+const cracha = document.querySelector('.cracha');
 const crachaFoto = document.getElementById('cracha-foto');
 const crachaNome = document.getElementById('cracha-nome');
 const crachaMatricula = document.getElementById('cracha-matricula');
-
-// Seleciona os elementos o crachá
-const cracha = document.querySelector('.cracha');
+const crachaGeral = document.getElementById('brm');
+const crachaJL = document.getElementById('brmjl');
 
 //Atualiza os elementos no crachá em tempo real
 inputFoto.addEventListener('change', () => {
@@ -22,26 +24,68 @@ inputFoto.addEventListener('change', () => {
   if (foto) {
     const reader = new FileReader();
     reader.readAsDataURL(foto);
-
     reader.onload = (event) => {
       crachaFoto.src = event.target.result;
     }
   }
 });
 
-inputNome.addEventListener('input', () => { crachaNome.textContent = inputNome.value });
+//Verifica se a primeira opção está marcada
+function verificaRadio() {
+  if(radio01.checked) {
+    crachaGeral.style.display = "block"; 
+    crachaJL.style.display = "none";
+  }
+};
 
+//Se a primeira opção estiver marcada, inicializa a pagina com o layout padrão
+document.addEventListener('DOMContentLoaded', () => {
+  verificaRadio();
+});
+
+//Se a opção "BR Marinas" for marcada, altera o layout do crachá para o da BR Marinas
+radio01.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    crachaGeral.style.display = "block"; 
+    crachaJL.style.display = "none";
+  }
+});
+
+//Se a opção "BR Marinas JL" for marcada, altera o layout do crachá para o da BR Marinas JL
+radio02.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    crachaJL.style.display = "block";
+    crachaGeral.style.display = "none"; 
+  }
+});
+
+//Só exibe a imagem se algum arquivo for carregado (oculta o icone de imagem quebrada)
+inputFoto.addEventListener('change', () => {
+  const foto = inputFoto.files[0];
+  if (foto) {
+    const reader = new FileReader();
+    reader.readAsDataURL(foto);
+    reader.onload = (event) => {
+      crachaFoto.src = event.target.result;
+      crachaFoto.style.display = 'block'; // 
+    }
+  } else {
+    crachaFoto.style.display = 'none'; 
+  }
+});
+
+
+//Insere o nome e matricula no crachá
+inputNome.addEventListener('input', () => { crachaNome.textContent = inputNome.value });
 inputMatricula.addEventListener('input', () => { crachaMatricula.textContent = inputMatricula.value });
 
 //Apaga os valores dos campos caso a página seja atualizada
 document.addEventListener('DOMContentLoaded', () => {
   apagaFoto.value = "";
 });
-
 document.addEventListener('DOMContentLoaded', () => {
   apagaNome.value = "";
 });
-
 document.addEventListener('DOMContentLoaded', () => {
   apagaMatricula.value = "";
 });
@@ -57,6 +101,7 @@ botaoBaixar.addEventListener('click', () => {
   });
 });
 
+/*
 //Imprime o crachá
 document.getElementById('print').addEventListener('click', () => {
   const cracha = document.querySelector('.cracha'); // Seleciona o crachá
@@ -94,3 +139,4 @@ document.getElementById('print').addEventListener('click', () => {
     document.body.removeChild(overlay);
   });
 });
+*/
